3fd0c0f52658265a04d1b1487b21873e84ebadf0
Paulo Henrique <phbduque@hotmail.com>
2024-08-17 00:03:20 -0300

    project working

---
+++ src/contexts/StockContext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
+
+export const StockContext = createContext({});
+
+StockContextProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export function StockContextProvider({ children }) {
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem("phbd-react-stock");
+    if (!storedItems) return [];
+    const items = JSON.parse(storedItems);
+    items.forEach((item) => {
+      item.createdAt = new Date(item.createdAt);
+      item.updatedAt = new Date(item.updatedAt);
+    });
+    return items;
+  });
+
+  const addItem = (item) => {
+    setItems((currentState) => {
+      const updatedItems = [item, ...currentState];
+      localStorage.setItem("phbd-react-stock", JSON.stringify(updatedItems));
+      return updatedItems;
+    });
+  };
+
+  const getItem = (itemId) => {
+    return items.find((item) => item.id === +itemId);
+  };
+
+  const updateItem = (itemId, newAttributes) => {
+    setItems((currentState) => {
+      const itemIndex = currentState.findIndex((item) => item.id === +itemId);
+      const updatedItems = [...currentState];
+      Object.assign(updatedItems[itemIndex], newAttributes, {
+        updatedAt: new Date(),
+      });
+      localStorage.setItem("phbd-react-stock", JSON.stringify(updatedItems));
+      return updatedItems;
+    });
+  };
+  const deleteItem = (itemId) => {
+    setItems((currentState) => {
+      const updatedItems = currentState.filter((item) => item.id !== itemId);
+      localStorage.setItem("phbd-react-stock", JSON.stringify(updatedItems));
+    });
+  };
+  const stock = {
+    items,
+    addItem,
+    deleteItem,
+    updateItem,
+    getItem,
+  };
+
+  return (
+    <StockContext.Provider value={stock}>{children}</StockContext.Provider>
+  );
+}
